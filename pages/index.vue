3e03c4503ce59c0855865a0023c9739d876d1e8d
Akramurridjal Rahman <xerod@outlook.com>
2020-02-22 00:42:31 +0700

    Switching to Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen">
+    <p>Hello there 👋</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {}
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
